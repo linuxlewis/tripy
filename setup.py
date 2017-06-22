457823e15ff5f14dc4fa6f2a4f7cc2f1669b7757
Sam Bolgert <sbolgert@gmail.com>
2017-06-22 13:54:19 -0700

    Fix Classifiers

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     license='MIT',
     packages=find_packages(exclude=["tests.*"]),
     classifiers=[
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ]
